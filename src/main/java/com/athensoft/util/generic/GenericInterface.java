fb0a7fb5cdc92f597bda080df8ef50bb2ba74c82
leon846666 <leon846666@gmail.com>
2018-02-27 16:58:23 -0500

    Generic interface at creation step

---
+++ src/main/java/com/athensoft/util/generic/GenericInterface.java
@@ -0,0 +1,14 @@
+package com.athensoft.util.generic;
+
+
+/**
+ * 
+ * */
+	
+public interface GenericInterface<T1,T2> {
+	
+	    public T1 showManyInfo(T1 t);
+	    public T2 displayOtherThins(T2 t);
+	    
+	    
+}
