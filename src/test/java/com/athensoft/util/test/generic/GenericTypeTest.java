7079c1af0a8f0edccda4db92218994ecd09d403d
leon846666 <leon846666@gmail.com>
2018-02-28 14:58:53 -0500

    modified

---
+++ src/test/java/com/athensoft/util/test/generic/GenericTypeTest.java
@@ -0,0 +1,38 @@
+package com.athensoft.util.test.generic;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.athensoft.util.generic.GenericsType;
+
+import junit.framework.Assert;
+
+public class GenericTypeTest {
+	
+	private GenericsType generic=new GenericsType();
+	
+
+	
+
+	@Test
+	public void testIntroduceGeneric() {
+		generic.introduceGeneric();
+	}
+
+	@Test
+	public void testAfterCompile() {
+		generic.afterCompile();	
+		}
+
+	@Test
+	public void testCustomizedType(){
+		generic.customizedType();
+		
+	}
+
+}
