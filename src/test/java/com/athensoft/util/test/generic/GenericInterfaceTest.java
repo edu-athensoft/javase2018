dc79f21d57af3d53d5a94e7f814ea79b83d68dc4
leon846666 <leon846666@gmail.com>
2018-03-01 15:58:05 -0500

    A test case for GenericInterface

---
+++ src/test/java/com/athensoft/util/test/generic/GenericInterfaceTest.java
@@ -0,0 +1,12 @@
+package com.athensoft.util.test.generic;
+
+public class GenericInterfaceTest {
+
+	
+	/**
+	 * There is not necessary to create a test case for Generic Interface 
+	 * Because in that interface mainly I just introduced the structure,
+	 * nothing practical.
+	 * 
+	 */
+}
